452c86a14751e6a2bb7e1ca98b400822d6741851
김동규 <tkillman@logiall.com>
2024-12-14 00:59:31 +0900

    cc

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+//Proxy 설정
+app.use(
+  '/', // 프록시 경로 (예: http://localhost:3000/proxy)
+  createProxyMiddleware({
+    //target: 'https://naver.com', // 타겟 웹사이트
+    target: 'https://top-shop.logiall.com', // 타겟 웹사이트
+    changeOrigin: true, // 호스트 헤더 변경
+    on: {
+      proxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('version', 'staging');
+      },
+    },
+    pathRewrite: {
+      '^/api': '/', // 경로 재작성
+    },
+  })
+);
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
