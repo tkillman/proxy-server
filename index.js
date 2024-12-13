3139243dfe6343f3b9188a9b6d199e22c9daec63
김동규 <tkillman@logiall.com>
2024-12-13 19:44:24 +0900

    finish

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(
   '/', // 프록시 경로 (예: http://localhost:3000/proxy)
   createProxyMiddleware({
-    target: 'https://naver.com', // 타겟 웹사이트
-    //target: 'https://top-shop.logiall.com', // 타겟 웹사이트
+    //target: 'https://naver.com', // 타겟 웹사이트
+    target: 'https://top-shop.logiall.com', // 타겟 웹사이트
     changeOrigin: true, // 호스트 헤더 변경
     on: {
       proxyReq: (proxyReq, req, res) => {
@@ -27,10 +27,10 @@ app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello world');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('hello world');
+// });
 
-app.get('/health', (req, res) => {
-  res.status(200).send('ok');
-})
+// app.get('/health', (req, res) => {
+//   res.status(200).send('ok');
+// })
