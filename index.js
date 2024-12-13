128279479f67d30c55e34e89f0b7b570d7fef6a9
김동규 <tkillman@logiall.com>
2024-12-13 19:35:02 +0900

    hh

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-// Proxy 설정
-// app.use(
-//   '/', // 프록시 경로 (예: http://localhost:3000/proxy)
-//   createProxyMiddleware({
-//     target: 'https://naver.com', // 타겟 웹사이트
-//     //target: 'https://top-shop.logiall.com', // 타겟 웹사이트
-//     changeOrigin: true, // 호스트 헤더 변경
-//     on: {
-//       proxyReq: (proxyReq, req, res) => {
-//         proxyReq.setHeader('version', 'staging');
-//       },
-//     },
-//     pathRewrite: {
-//       '^/': '/', // 경로 재작성
-//     },
-//   })
-// );
+//Proxy 설정
+app.use(
+  '/', // 프록시 경로 (예: http://localhost:3000/proxy)
+  createProxyMiddleware({
+    target: 'https://naver.com', // 타겟 웹사이트
+    //target: 'https://top-shop.logiall.com', // 타겟 웹사이트
+    changeOrigin: true, // 호스트 헤더 변경
+    on: {
+      proxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('version', 'staging');
+      },
+    },
+    pathRewrite: {
+      '^/': '/', // 경로 재작성
+    },
+  })
+);
 
 // 기본 포트 설정
 const PORT = 3000;
