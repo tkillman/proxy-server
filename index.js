52de88b09481ef8398a0838c6909c3cf42ad0e1c
김동규 <tkillman@logiall.com>
2024-12-13 20:53:17 +0900

    v

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-// app.use(
-//   '/', // 프록시 경로 (예: http://localhost:3000/proxy)
-//   createProxyMiddleware({
-//     //target: 'https://naver.com', // 타겟 웹사이트
-//     target: 'https://daum.net', // 타겟 웹사이트
-//     changeOrigin: true, // 호스트 헤더 변경
-//     on: {
-//       proxyReq: (proxyReq, req, res) => {
-//         proxyReq.setHeader('version', 'staging');
-//       },
-//     },
-//     pathRewrite: {
-//       '^/': '/', // 경로 재작성
-//     },
-//   })
-// );
+app.use(
+  '/', // 프록시 경로 (예: http://localhost:3000/proxy)
+  createProxyMiddleware({
+    //target: 'https://naver.com', // 타겟 웹사이트
+    target: 'https://top-shop.logiall.com', // 타겟 웹사이트
+    changeOrigin: true, // 호스트 헤더 변경
+    on: {
+      proxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('version', 'staging');
+      },
+    },
+    pathRewrite: {
+      '^/': '/', // 경로 재작성
+    },
+  })
+);
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello world');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('hello world');
+// });
