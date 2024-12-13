df6d11719bb1ac90724030fa28088337974f7d18
김동규 <tkillman@logiall.com>
2024-12-13 20:10:44 +0900

    a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("main")
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on http://localhost:${PORT}`);
+});
