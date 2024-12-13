c51b9da9bb92a8fb2fa007908e1611f486f92316
김동규 <tkillman@logiall.com>
2024-12-13 19:10:52 +0900

    health 체크

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Proxy 설정
-app.use(
-  '/', // 프록시 경로 (예: http://localhost:3000/proxy)
-  createProxyMiddleware({
-    //target: 'https://naver.com', // 타겟 웹사이트
-    target: 'https://top-shop.logiall.com', // 타겟 웹사이트
-    changeOrigin: true, // 호스트 헤더 변경
-    on: {
-      proxyReq: (proxyReq, req, res) => {
-        proxyReq.setHeader('version', 'staging');
-      },
-    },
-    pathRewrite: {
-      '^/': '/', // 경로 재작성
-    },
-  })
-);
+// app.use(
+//   '/', // 프록시 경로 (예: http://localhost:3000/proxy)
+//   createProxyMiddleware({
+//     //target: 'https://naver.com', // 타겟 웹사이트
+//     target: 'https://top-shop.logiall.com', // 타겟 웹사이트
+//     changeOrigin: true, // 호스트 헤더 변경
+//     on: {
+//       proxyReq: (proxyReq, req, res) => {
+//         proxyReq.setHeader('version', 'staging');
+//       },
+//     },
+//     pathRewrite: {
+//       '^/': '/', // 경로 재작성
+//     },
+//   })
+// );
 
 // 기본 포트 설정
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
+
+app.get('/health', (req, res) => {
+  res.send('ok');
+})
