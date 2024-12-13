f9ea2528e6f8dc17547b2c99a67ce64b0cf00828
김동규 <tkillman@logiall.com>
2024-12-13 17:26:21 +0900

    proxy 설정제거

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Proxy 설정
-app.use(
-  '/', // 프록시 경로 (예: http://localhost:3000/proxy)
-  createProxyMiddleware({
-    target: 'https://top-shop.logiall.com', // 타겟 웹사이트
-    changeOrigin: true, // 호스트 헤더 변경
-    on: {
-      proxyReq: (proxyReq, req, res) => {
-        proxyReq.setHeader('version', 'staging');
-      },
-    },
-  })
-);
+// app.use(
+//   '/', // 프록시 경로 (예: http://localhost:3000/proxy)
+//   createProxyMiddleware({
+//     target: 'https://top-shop.logiall.com', // 타겟 웹사이트
+//     changeOrigin: true, // 호스트 헤더 변경
+//     on: {
+//       proxyReq: (proxyReq, req, res) => {
+//         proxyReq.setHeader('version', 'staging');
+//       },
+//     },
+//   })
+// );
 
 // 기본 포트 설정
 const PORT = 3000;
