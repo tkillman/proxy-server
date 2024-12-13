e0a13f987594c1c5ab3bf0e859a70a662eec7c7c
김동규 <tkillman@logiall.com>
2024-12-13 20:54:44 +0900

    z

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ app.use(
   '/', // 프록시 경로 (예: http://localhost:3000/proxy)
   createProxyMiddleware({
     //target: 'https://naver.com', // 타겟 웹사이트
-    target: 'https://top-shop.logiall.com', // 타겟 웹사이트
-    changeOrigin: true, // 호스트 헤더 변경
-    on: {
-      proxyReq: (proxyReq, req, res) => {
-        proxyReq.setHeader('version', 'staging');
-      },
-    },
-    pathRewrite: {
-      '^/': '/', // 경로 재작성
-    },
+    // target: 'https://top-shop.logiall.com', // 타겟 웹사이트
+    // changeOrigin: true, // 호스트 헤더 변경
+    // on: {
+    //   proxyReq: (proxyReq, req, res) => {
+    //     proxyReq.setHeader('version', 'staging');
+    //   },
+    // },
+    // pathRewrite: {
+    //   '^/': '/', // 경로 재작성
+    // },
   })
 );
 
