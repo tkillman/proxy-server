bdc1e70ab463668dd836c9ca01bba59988d0e12d
김동규 <tkillman@logiall.com>
2024-12-14 10:49:10 +0900

    zczc

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,15 @@
-const express = require('express');
+import express, { Express, Request, Response } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
+const proxyMiddleware = createProxyMiddleware<Request, Response>({
+  target: 'http://top-shop.logiall.com',
+  changeOrigin: true,
+});
+
 app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+app.use('/', proxyMiddleware);
 
 module.exports = app;
